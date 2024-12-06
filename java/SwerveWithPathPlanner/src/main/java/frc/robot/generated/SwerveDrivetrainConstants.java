80cca741e767a3cf36a2975ee16db0ea55e93cd0
bigbluerobotics <mdicks2025@pingry.org>
2024-12-06 13:46:52 -0800

    pdafsmkl;amfsdkl[p

--- java/SwerveWithPathPlanner/src/main/java/frc/robot/generated/SwerveDrivetrainConstants.java
+++ java/SwerveWithPathPlanner/src/main/java/frc/robot/generated/SwerveDrivetrainConstants.java
@@ -1,6 +1,3 @@
-Below is the full updated code integrating the NavX2-MXP setup with your swerve drive configuration. The implementation uses the [Studica Robotics NavX library](https://github.com/Studica-Robotics/NavX) and replaces the Pigeon2 setup in your original code:
-
-```java
 package frc.robot.generated;
 
 import static edu.wpi.first.units.Units.*;
@@ -73,7 +70,6 @@ public class TunerConstants {
     private static final Voltage kDriveFrictionVoltage = Volts.of(0.25);
 
     public static final SwerveDrivetrainConstants DrivetrainConstants = new SwerveDrivetrainConstants()
-            .withCANBusName(kCANBus.getName())
             .withNavX(navx) // Configure NavX instead of Pigeon
             .withNavXOffset(kNavXOffset);
 
@@ -150,10 +146,10 @@ public class TunerConstants {
     public static final SwerveModuleConstants BackRight = ConstantCreator.createModuleConstants(
             kBackRightSteerMotorId, kBackRightDriveMotorId, kBackRightEncoderId, kBackRightEncoderOffset,
             kBackRightXPos, kBackRightYPos, kInvertRightSide, kBackRightSteerMotorInverted, kBackRightCANcoderInverted);
-
+        
     /**
      * Creates a CommandSwerveDrivetrain instance.
-     * This should only be called once in your robot program.
+     * This should only be called once in your robot program,.
      */
     public static CommandSwerveDrivetrain createDrivetrain() {
         return new CommandSwerveDrivetrain(
@@ -161,3 +157,101 @@ public class TunerConstants {
         );
     }
 }
+
+/*/*
+ * Copyright (C) Cross The Road Electronics.  All rights reserved.
+ * License information can be found in CTRE_LICENSE.txt
+ * For support and suggestions contact support@ctr-electronics.com or file
+ * an issue tracker at https://github.com/CrossTheRoadElec/Phoenix-Releases
+ */
+package com.ctre.phoenix6.swerve;
+
+import com.ctre.phoenix6.configs.Pigeon2Configuration;
+import com.ctre.phoenix6.swerve.jni.SwerveJNI;
+
+/**
+ * Common constants for a swerve drivetrain.
+ */
+public class SwerveDrivetrainConstants {
+    /**
+     * Name of the CAN bus the swerve drive is on. Possible CAN bus strings are:
+     * 
+     * <ul>
+     *   <li> empty string or "rio" for the native roboRIO CAN bus
+     *   <li> CANivore name or serial number
+     *   <li> "*" for any CANivore seen by the program
+     * </ul>
+     * 
+     * Note that all devices must be on the same CAN bus.
+     */
+    public String CANBusName = "rio";
+    /**
+     * CAN ID of the Pigeon2 on the drivetrain.
+     */
+    public int Pigeon2Id = 0;
+    /**
+     * The configuration object to apply to the Pigeon2. This defaults to null. If
+     * this remains null, then the Pigeon2 will not be configured (and whatever
+     * configs are on it remain on it). If this is not null, the Pigeon2 will be
+     * overwritten with these configs.
+     */
+    public Pigeon2Configuration Pigeon2Configs = null;
+    
+    /**
+     * Modifies the CANBusName parameter and returns itself.
+     * <p>
+     * Name of the CAN bus the swerve drive is on. Possible CAN bus strings are:
+     * 
+     * <ul>
+     *   <li> empty string or "rio" for the native roboRIO CAN bus
+     *   <li> CANivore name or serial number
+     *   <li> "*" for any CANivore seen by the program
+     * </ul>
+     * 
+     * Note that all devices must be on the same CAN bus.
+     *
+     * @param newCANBusName Parameter to modify
+     * @return this object
+     */
+    public SwerveDrivetrainConstants withCANBusName(String newCANBusName) {
+        this.CANBusName = newCANBusName;
+        return this;
+    }
+    
+    /**
+     * Modifies the Pigeon2Id parameter and returns itself.
+     * <p>
+     * CAN ID of the Pigeon2 on the drivetrain.
+     *
+     * @param newPigeon2Id Parameter to modify
+     * @return this object
+     */
+    public SwerveDrivetrainConstants withPigeon2Id(int newPigeon2Id) {
+        this.Pigeon2Id = newPigeon2Id;
+        return this;
+    }
+    
+    /**
+     * Modifies the Pigeon2Configs parameter and returns itself.
+     * <p>
+     * The configuration object to apply to the Pigeon2. This defaults to null. If
+     * this remains null, then the Pigeon2 will not be configured (and whatever
+     * configs are on it remain on it). If this is not null, the Pigeon2 will be
+     * overwritten with these configs.
+     *
+     * @param newPigeon2Configs Parameter to modify
+     * @return this object
+     */
+    public SwerveDrivetrainConstants withPigeon2Configs(Pigeon2Configuration newPigeon2Configs) {
+        this.Pigeon2Configs = newPigeon2Configs;
+        return this;
+    }
+    
+    long createNativeInstance() {
+        return SwerveJNI.JNI_CreateDrivetrainConstants(
+            CANBusName,
+            Pigeon2Id
+        );
+    }
+}
+ */
